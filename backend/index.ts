219870a9b41a6e18341642615eed6cf6e53396e5
thanosoncode <thanosoncode@gmail.com>
2023-08-05 07:49:02 +0300

    remove frontend send

--- backend/index.ts
+++ backend/index.ts
@@ -13,18 +13,9 @@ app.use(cors());
 
 app.use("/api/workouts", router);
 
-if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running....");
-  });
-}
+app.get("/", (req, res) => {
+  res.send("API is running....");
+});
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
