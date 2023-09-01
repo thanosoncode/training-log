517c714f90fb46949a70c847db7f43f4117680e1
thanosoncode <thanosoncode@gmail.com>
2023-09-01 12:45:32 +0300

    update api url

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? 'https://training-log.onrender.com' : ''
+  baseURL: process.env.NODE_ENV === 'production' ? 'https://training-log-api.vercel.app/' : ''
 });
 
 export default axiosInstance;
