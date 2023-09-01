56dd1abb107a623ec25beace453e7166455f0d83
thanosoncode <thanosoncode@gmail.com>
2023-09-01 12:50:05 +0300

    correct url

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? 'https://training-log-api.vercel.app/' : ''
+  baseURL: process.env.NODE_ENV === 'production' ? 'https://training-log-be.vercel.app/' : ''
 });
 
 export default axiosInstance;
