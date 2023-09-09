9ea720b636178e8cb0af97f5d942276a7d9cf8ce
Thanos <74588018+thanosoncode@users.noreply.github.com>
2023-09-09 10:19:13 +0300

    production url

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? 'https://training-log-be.vercel.app/' : ''
+  baseURL: process.env.NODE_ENV === 'production' ? process.env.PRODUCTION_URL : ''
 });
 
 export default axiosInstance;
