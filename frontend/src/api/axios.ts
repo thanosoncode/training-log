f3412040602bf2ae472730957b7c151ab3d1962b
Thanos <74588018+thanosoncode@users.noreply.github.com>
2023-09-09 10:50:48 +0300

    add headers'

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -2,7 +2,11 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: process.env.NODE_ENV === 'production' ? process.env.PRODUCTION_URL : '',
-  withCredentials: true
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json'
+  }
 });
 
 export default axiosInstance;
