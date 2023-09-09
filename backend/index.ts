c65450e61048c342e8555b3447f195552c0111de
Thanos <74588018+thanosoncode@users.noreply.github.com>
2023-09-09 10:36:11 +0300

    remove options

--- backend/index.ts
+++ backend/index.ts
@@ -11,20 +11,8 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 5000;
 
-const allowedOrigins: string[] = ["https://traininglog.netlify.app"];
-
-const corsOptions: CorsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin as string) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
-
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use("/api/strength", strengthRouter);
 app.use("/api/cardio", cardioRouter);
